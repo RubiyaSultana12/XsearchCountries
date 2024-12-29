bc21366e15e8c310639ffaf85d71c382a8aeba33
RubiyaSultana12 <rubiya.sultana22@gmail.com>
2024-12-29 10:10:52 +0530

    test-commit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,17 @@ console.log(countries);
 const api=" https://restcountries.com/v3.1/all";
 
 const[search, setSearch]=useState("");
+
 const[filterr, setFilter]=useState([]);
 
 
 useEffect(()=>{
   fetch(api)
-    .then((res)=>res.json())
+    .then((res)=>{
+    if(!res.ok){
+      throw new Error("Network issue")
+    }
+      return res.json()})
     .then((data)=>setCountries(data))
     .catch((error)=>console.error("Error fetching data:" + error));
 },[]);
@@ -22,7 +27,7 @@ const handleSearch=(e)=>{
   setSearch(searchterm);
 
   const filterCountries=countries.filter((country)=>
-    country.name.common.toLowerCase().includes(searchterm));
+    country.name.common.toLowerCase().includes(searchterm.toLowerCase()));
     setFilter(filterCountries);
     
 }
@@ -30,15 +35,15 @@ const handleSearch=(e)=>{
 
 
   return (
-    <div className="App">
-      <div className="inputBox">
+    <div class="App">
+      <div class="inputBox">
       <input type="text" placeholder="search for countries"
       value={search}
       onChange={handleSearch}/>
       </div>
    <div className="container">
   {filterr.map((country) => (
-       <div key={country.index} className="countryCard">
+       <div key={country.cca3} class="countryCard">
   <img
        src={country.flags.png}
        alt={`Flag of ${country.name.common}`}
